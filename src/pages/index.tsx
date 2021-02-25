ddfb72f9ac871e73e5315c5b74a963dc529b0645
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2021-02-25 00:16:38 -0300

    Showing challenges, implementing context and basic interations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 
 import styles from '../styles/Pages/Home.module.css';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -22,7 +23,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
