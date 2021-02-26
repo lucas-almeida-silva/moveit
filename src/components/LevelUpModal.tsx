def20f428cb8a886bb90254281de2f9a612d89a2
Lucas Almeida da Silva <lucas_almeidasilva01@hotmail.com>
2021-02-26 18:29:15 -0300

    Saving user info at cookie storage and showing level up modal

---
+++ src/components/LevelUpModal.tsx
@@ -0,0 +1,38 @@
+import { useContext, useEffect } from 'react'
+import { animated } from 'react-spring';
+
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from '../styles/components/LevelUpModal.module.css'
+
+interface LevelUpModalProps {
+  style: object;
+}
+
+export function LevelUpModal({ style }: LevelUpModalProps) {
+  const { level, closeLevelUpModal } = useContext(ChallengesContext);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      closeLevelUpModal();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <animated.div className={styles.overlay} style={style}>
+      <div className={styles.container}>
+        <header>{level}</header>
+
+        <strong>Parabéns</strong>
+        <p>Você alcançou um novo nível</p>
+
+        <button type="button" onClick={closeLevelUpModal}>
+          <img src="/icons/close.svg" alt="Fechar modal" />
+        </button>
+      </div>
+    </animated.div>
+  );
+}
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,5 +1,9 @@
 import { createContext, useState, ReactNode, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import challenges from '../../challenges.json';
+
+import { LevelUpModal } from '../components/LevelUpModal';
+import { useTransition } from 'react-spring';
 
 interface Challenge {
   type: 'body' | 'eye';
@@ -17,20 +21,30 @@ interface ChallengesContextData {
   startNewChallenge: () => void;
   resetChallenge: () => void;
   completeChallenge: () => void;
+  closeLevelUpModal: () => void;
 }
-
 interface ChallengesProviderProps {
   children: ReactNode
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-  const [level, setLevel] = useState(1);
-  const [currentExperience, setCurrentExperience] = useState(0);
-  const [challengesCompleted, setChallengesCompleted] = useState(0);
+export function ChallengesProvider({ children, ...rest }: ChallengesProviderProps) {
+  const [level, setLevel] = useState(rest.level ?? 1);
+  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
 
   const [activeChallenge, setActiveChallenge] = useState(null);
+  const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false);
+
+  const levelUpModalTransition = useTransition(isLevelUpModalOpen, null, {
+    from: { opacity: 0, transform: 'translateX(-50%)'},
+    enter: { opacity: 1, transform: 'translateX(0%)'},
+    leave: { opacity: 0, transform: 'translateX(-50%)'}
+  })
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
@@ -38,8 +52,19 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     Notification.requestPermission();
   }, []);
 
+  useEffect(() => {
+    Cookies.set('level', String(level));
+    Cookies.set('currentExperience', String(currentExperience));
+    Cookies.set('challengesCompleted', String(challengesCompleted));
+  }, [level, currentExperience, challengesCompleted]);
+
   function levelUp() {
     setLevel(level + 1);
+    setIsLevelUpModalOpen(true);
+  }
+
+  function closeLevelUpModal() {
+    setIsLevelUpModalOpen(false);
   }
 
   function startNewChallenge() {
@@ -91,10 +116,16 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         startNewChallenge,
         activeChallenge,
         resetChallenge,
-        completeChallenge 
+        completeChallenge,
+        closeLevelUpModal 
       }}
     >
       {children}
+
+      { levelUpModalTransition.map(({ item, key, props }) => 
+        item && <LevelUpModal key={key} style={props}/>
+      )}
+
     </ChallengesContext.Provider>
   )
 }
